d0c0264bae455a0382886b68c3b818efd210c68b
SeungJoo Han <sugarcanes0118@gmail.com>
2023-03-13 15:03:59 +0900

    Update Feature Matching.py

--- Feature Matching.py
+++ Feature Matching.py
@@ -2,8 +2,8 @@
 
 import cv2, numpy as np
 
-img1 = cv2.imread('../img/taekwonv1.jpg')
-img2 = cv2.imread('../img/figures.jpg')
+img1 = cv2.imread('./Image1.jpg')
+img2 = cv2.imread('./Image2.jpg')
 gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
 gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
 
@@ -21,6 +21,6 @@ matches = matcher.match(desc1, desc2)
 res = cv2.drawMatches(img1, kp1, img2, kp2, matches, None, \
                       flags=cv2.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
 # 결과 출력 
-cv2.imshow('BFMatcher + SIFT', res)
+cv2.imshow('BeFMatcher + SIFT', res)
 cv2.waitKey()
 cv2.destroyAllWindows()
